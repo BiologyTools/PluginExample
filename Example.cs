4fa9fd9f79d5809b752e81ab59a291cf9e08ef10
RepoErik <84872500+BiologyTools@users.noreply.github.com>
2024-01-04 18:42:50 +0200

    Plugin Improvements.

--- Example.cs
+++ Example.cs
@@ -1,4 +1,7 @@
-﻿using BioGTK;
+﻿using AForge;
+using BioGTK;
+using com.sun.org.apache.xml.@internal.resolver.helpers;
+using Gtk;
 namespace PluginExample
 {
     public class PluginExample : BioGTK.Plugin.IPlugin
@@ -7,12 +10,40 @@ namespace PluginExample
         public string MenuPath => "Tools/" + Name + ".dll";
         public bool ContextMenu => false;
         ImageView v;
-        public void Execute()
+        public void Execute(string[] args)
         {
             //Since we will be using the GUI we call App.Initialize();
             BioImage bm = BioImage.OpenFile("F:\\TESTIMAGES\\CZI\\16Bit-ZStack.czi",0,false,false);
             v = ImageView.Create(bm);
             v.Show();
+        }
+        public void KeyUpEvent(object o, KeyPressEventArgs e)
+        {
+
+        }
+        public void KeyDownEvent(object o, KeyPressEventArgs e)
+        {
+
+        }
+        public void ScrollEvent(object o, ScrollEventArgs args)
+        {
+
+        }
+        public void Drawn(object o, DrawnArgs e)
+        {
+
+        }
+        public void MouseMove(object o, PointD e, MotionNotifyEventArgs buts)
+        {
+
+        }
+        public void MouseUp(object o, PointD e, ButtonReleaseEventArgs buts)
+        {
+
+        }
+        public void MouseDown(object o, PointD e, ButtonPressEventArgs buts)
+        {
+
         }
     }
 }
